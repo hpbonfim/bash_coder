882183abb37ad90c918fc1eccda04fad4ab9350b
Henrique <hp_bonfim@hotmail.com>
2020-10-07 10:48:10 -0300

    --allow-empty-message

--- dates.sh
+++ dates.sh
@@ -1,9 +1,9 @@
 while read date
 do
     fileName=`echo "$date" | tr " " "_"`
-    date="$date 12:37 2020 +0300"
+    date="$date 12:37 2009 +0300"
     echo "Creating file... $fileName"
     touch "$fileName"
     git add "$fileName"
-    git commit --date="$date" --author="hp_bonfim@hotmail.com" -m "$fileName"
+    git commit --date="$date" --author="hpbonfim" -m "$fileName"
 done <dates.txt
